e65e0c2d1e090db457a860fa13bc0e46213b98ad
NUYA193 <guyigj636@gmail.com>
2025-04-27 17:07:46 +0300

    Update main.py
    Переход к ООП

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import random
 
-def wer(a,b):
-    c=random.randint(a,b)
-    return c
-  
+class Randomazer:
+    def __init__(self,x,y):
+        self.border1 = x
+        self.border2 = y
+        self.value = self.__get_random_value()
+
+    def __get_random_value(self):
+        return random.randint(self.border1,self.border2)
+
+    def get_value(self):
+        return self.value
+
 
 a=int(input("Введите число"))
 b=int(input("Введите число"))
-
-res = wer(a,b)
-print("Ваше число:",res)
+randomazer = Randomazer(a,b)
+result = randomazer.get_value()
+print("Ваше число:",result)
